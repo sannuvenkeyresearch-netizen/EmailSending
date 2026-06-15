98824f7863eb42170b23ec96ba1b1a8eee96def3
sannuvenkeyresearch-netizen <sannuvenkey.research@gmail.com>
2026-06-15 14:43:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const transporter = nodemailer.createTransport({
+nst transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
@@ -27,15 +27,46 @@ app.get("/", (req, res) => {
     res.send("Email API is Running Successfully");
 });
 
-// Email API
 app.post("/send-email", async (req, res) => {
 
-    console.log("API HIT");
+    console.log("========== API HIT ==========");
+    console.log(req.body);
 
-    return res.status(200).json({
-        success: true,
-        message: "API Working"
-    });
+    try {
+
+        const { to, subject, message } = req.body;
+
+        // Verify SMTP connection
+        await transporter.verify();
+        console.log("SMTP Connected Successfully");
+
+        // Send Email
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to,
+            subject,
+            html: `<p>${message}</p>`
+        });
+
+        console.log("MAIL SENT SUCCESSFULLY");
+
+        return res.status(200).json({
+            success: true,
+            message: "Email sent successfully"
+        });
+
+    } catch (error) {
+
+        console.log("MAIL ERROR");
+        console.log(error);
+
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+});
+
 
 });
 const PORT = process.env.PORT || 3000;
